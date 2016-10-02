64a546cf7eb978a87debcf32d1f11b635bd55f62
Bogdan Solga <bogdan.solga@gmail.com>
2016-10-02 17:45:52 +0300

    [clean] Removed the old project structure

--- w02d04/maven-build/src/main/java/com/msgsystems/training/w02d04/maven/model/AbstractProduct.java
+++
@@ -1,8 +0,0 @@
-package com.msgsystems.training.w02d04.maven.model;
-
-public abstract class AbstractProduct {
-
-    public abstract int getId();
-
-    public abstract void setId(final int id);
-}
